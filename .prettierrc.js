f9a792a1d06ebe29508256806673ed1747579a8c
Mark Stacey <markjstacey@gmail.com>
2024-11-30 17:58:25 -0330

    Add example file, prettier, and plugin

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 140,
+  tabWidth: 4,
+  useTabs: false,
+  endOfLine: 'auto',
+  // plugins: ['prettier-plugin-sort-json'],
+};
